c08009778dfe1d2f5844adcfb216211c6b72378f
willson <willsoncatacora1@gmail.com>
2024-10-17 16:03:01 -0300

    implement api data.

--- screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,5 +1,6 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.service.ConsumirAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
@@ -24,5 +25,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		ConvierteDatos conversor = new ConvierteDatos();
 		var datos = conversor.obtenerDatos(json, DatosSerie.class);
 		System.out.println(datos);
+
+		json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=49827a15");
+		var episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
+		System.out.println(episodios);
 	}
 }
